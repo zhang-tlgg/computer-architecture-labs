b1b6cd8a660ffc880dc3516a9687cc6e61dea63b
Tianle Zhang <2446588658@qq.com>
2024-06-23 06:35:58 +0800

    cache block ass

--- cache-exp/cache_size.cpp
+++ cache-exp/cache_size.cpp
@@ -13,31 +13,29 @@ unsigned MeasureCacheSize([[maybe_unused]] ProcessorAbstract *p) {
     // The size will be ranged from 512 bytes to 4096 bytes(or 4 KB), and must be a power of 2
     // Return the accurate value
 
-    return 512;
+    unsigned testSize[5] = {512, 1024, 2048, 4096, 8192};
+    unsigned testTime[5];
 
-    // unsigned testSize[5] = {512, 1024, 2048, 4096, 8192};
-    // unsigned testTime[5];
+    for (int i = 0; i < 5; i++) {
+        testTime[i] = execute(p, "./test/sample_cache_size", 1, testSize[i]);
+    }
+    for (int i = 0; i < 5; i++) {
+        Logger::Warn(
+            "With test cache size = %u, program simulator ran %u cycles.",
+            testSize[i],
+            testTime[i]);
+    }
 
-    // for (int i = 0; i < 5; i++) {
-    //     testTime[i] = execute(p, "./test/sample_cache_size", 1, testSize[i]);
-    // }
-    // for (int i = 0; i < 5; i++) {
-    //     Logger::Warn(
-    //         "With test cache size = %u, program simulator ran %u cycles.",
-    //         testSize[i],
-    //         testTime[i]);
-    // }
+    int mx = -1;
+    int idx = 0;
 
-    // int mx = -1;
-    // int idx = 0;
+    for (int i = 0; i < 4; i++) {
+        int delta = ((int) testTime[i + 1]) - ((int) testTime[i]);
+        if (delta > mx) {
+            mx = delta;
+            idx = i;
+        }
+    }
 
-    // for (int i = 0; i < 4; i++) {
-    //     int delta = ((int) testTime[i + 1]) - ((int) testTime[i]);
-    //     if (delta > mx) {
-    //         mx = delta;
-    //         idx = i;
-    //     }
-    // }
-
-    // return testSize[idx];
+    return testSize[idx];
 }
